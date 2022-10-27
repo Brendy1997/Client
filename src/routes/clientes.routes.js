beca84c9ed93d8eaf3d1fccba97ea9a42eec20cc
Brendy1997 <btamayooca@gmail.com>
2022-10-26 23:26:17 -0400

    start

--- src/routes/clientes.routes.js
+++ src/routes/clientes.routes.js
@@ -1,10 +1,10 @@
 import { Router } from 'express'
-import { getCliente } from './clientes.controllers.js'
+
 import {createCliente} from './clientes.controllers.js'
 
 const router = Router()
 
-router.get('/clientes', getCliente);
+
 
 router.post('/clientes', createCliente);
 
