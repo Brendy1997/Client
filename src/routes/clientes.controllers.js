5c28be3ca2f9ad4e81f82e1b6a1943f9ee906d17
Brendy1997 <btamayooca@gmail.com>
2022-10-26 23:18:40 -0400

    start

--- src/routes/clientes.controllers.js
+++ src/routes/clientes.controllers.js
@@ -1,7 +1,8 @@
 import {pool} from '../db.js'
 
-export const getCliente = (req, res) => {
-    res.json('get');
+export const getCliente = async (req, res) => {
+    const [rows] = await pool.query('SELECT * FROM cliente')
+    res.json(rows);
 }
 
 export const createCliente = async (req, res) => {
@@ -9,5 +10,5 @@ export const createCliente = async (req, res) => {
     const { id, name, phone, age, zipcode, married, income } = req.body;
     const cli = { id, name, phone, age, zipcode, married, income };
     const result = await pool.query('INSERT INTO cliente SET ?', cli);
-    res.json("result");
+    res.json(result);
 }
